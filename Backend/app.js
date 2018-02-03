68217cbc50dd652f1ef65594faec9e5e5a24e3be
RamyBadawy <RamyBadawy81@gmail.com>
2018-02-03 13:00:37 -0600

    Project db connection updated

--- Backend/app.js
+++ Backend/app.js
@@ -17,14 +17,11 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 let db = null;
 app.use((req,res,next)=> {
@@ -34,16 +31,17 @@ app.use((req,res,next)=> {
         next();
     }
     else {
-        mongoClient.connect("mongodb://ramy_mwp:ramycs572@ds125068.mlab.com:25068/survey572", (err, client) => {
-            "use strict";
-            if (err) throw new Error('Can\'t connect to secret database');
+        mongoClient.connect("mongodb:// ramymwp:ramymwp @ds125068.mlab.com:25068/survey572", (err, client) => {
+            if (err) throw new Error('Can\'t connect to survey database');
             db = client.db('ramy');
             req.db = db;
             next();
         });
-
     }
 });
+
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,4 +61,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(4000);
 module.exports = app;
